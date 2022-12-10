625a23bfd30cdb48a4868ecdf4804069bd80b848
toh995 <toh995@gmail.com>
2022-12-10 00:08:54 -0800

    2022 day10

---
+++ 2022-haskell/app/Day10.hs
@@ -0,0 +1,73 @@
+module Day10 where
+
+import Data.List
+import Data.List.Split
+import Data.Maybe
+import Text.Read
+
+type Line = String
+type Instruction = (Int -> Int)
+type Value = Int
+
+----------------------
+-- SECTION: Main IO --
+----------------------
+filePath :: String
+filePath = "data/Day10.txt"
+
+main :: IO ()
+main = do
+  inputStr <- readFile filePath
+  let instructions = parseLines . lines $ inputStr
+  let values = computeValues instructions
+
+  let part1Answer = part1 values
+  putStrLn $ "PART 1: " ++ show part1Answer
+
+  let part2Answer = part2 values
+  putStrLn $ "PART 2:"
+  putStrLn part2Answer
+
+----------------------
+-- SECTION: Parsing --
+----------------------
+parseLines :: [Line] -> [Instruction]
+parseLines = concat . mapMaybe parseLine
+
+parseLine :: Line -> Maybe [Instruction]
+parseLine l =
+  case (words l) of
+       ["noop"] -> Just [id]
+       ["addx", strNum] -> (:) id <$> (singleton . (+) <$> readMaybe strNum)
+       _ -> Nothing
+
+computeValues :: [Instruction] -> [Value]
+computeValues = scanl' (flip ($)) 1
+
+---------------------
+-- SECTION: Part 1 --
+---------------------
+part1 :: [Value] -> Int
+part1 values =
+  sum
+  . map (computeSignalStrength values)
+  $ [20, 60, 100, 140, 180, 220]
+
+computeSignalStrength :: [Value] -> Int -> Int
+computeSignalStrength values i = i * values !! (i-1)
+
+---------------------
+-- SECTION: Part 2 --
+---------------------
+part2 :: [Value] -> String
+part2 values = buildDisplayString
+             . map computePixelChar
+             $ zip values (cycle [0..39])
+
+computePixelChar :: (Value, Int) -> Char
+computePixelChar (v, idx)
+  | abs (v - idx) <= 1 = '#'
+  | otherwise          = '.'
+
+buildDisplayString :: [Char] -> String
+buildDisplayString = unlines . chunksOf 40
--- 2022-haskell/app/Main.hs
+++ 2022-haskell/app/Main.hs
@@ -1,19 +1,7 @@
 module Main where
 
-import Data.List
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Day09 hiding (main)
-import qualified Day09
+import Day10 hiding (main)
+import qualified Day10
 
 main :: IO ()
--- main = print "hello"
-
--- main = Day09.main
-
-main = do
-  inputStr <- readFile filePath
-  let directions = parseLines . lines $ inputStr
-
-  mapM_ print $ executeDirections 10 directions
-  print $ Set.size . Set.fromList . getTails $ executeDirections 10 directions
+main = Day10.main
